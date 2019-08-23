45eeaee2c985af52ad625e37ff316345f80cabc5
Lighting <995289312@qq.com>
2019-08-23 21:17:43 +0800

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 database_config = {
   "schema": "crypt_schema",
-  "table": "BoxScore",
   "columns": ['`team_abbr`','`team_div`','`play_stat`','`play_pos`', '`play_height`', '`play_weight`', '`play_PF`'],
   "host": "127.0.0.1",
   "user": "root",
